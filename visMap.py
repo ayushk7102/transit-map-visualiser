9ced09039467e2964d161d9e31faef4ef34e085a
Ayush Kumar <ayushk7102@gmail.com>
2022-12-12 01:29:20 +0530

    Using routes instead of trips for drawing links

--- visMap.py
+++ visMap.py
@@ -44,6 +44,7 @@ for pt in stop_pts:
 route_trips = {}        # dictionary with key: route-id, value is a list of trip-ids
 trip_routes = {}        # dictionary with key: trip-id, value is a the mapped route-id
 
+
 for t in trips:
     if route_trips.get(t.route_id, 'nf') == 'nf':
         route_trips[t.route_id] = [t.id]
@@ -68,14 +69,25 @@ for s in stoptimes:
         trip_stops[s.trip_id].stops.append(s.stop_id)
 
 
-for k in trip_stops.keys():
-	# print(k)
-	rt_stps = trip_stops[k].stops
+route_stops = {}         # dictionary with key: route_id, value: [stop1, stop2..])
 
+for rt_k in route_trips.keys():
+	print('ROUTE : ', rt_k)
+	trip_0 = route_trips[rt_k][0]
+	route_stops[rt_k] = trip_stops[trip_0].stops
+
+
+ni = 0
+n = len(route_stops)
+for k in route_stops.keys():
+	# print(k)
+	rt_stps = route_stops[k]
+	print("Trip {0}/{1}".format(ni, n))
+	nstops = len(rt_stps)
 	for i in range(len(rt_stps)-1):
 		# print('here ', stops_loc_dict[rt_stps[i]])
 		try:
-			print(stops_loc_dict[rt_stps[i]],'--->',stops_loc_dict[rt_stps[i+1]])
+			print("Stop {0}/{1}".format(i, nstops) , '\t',stops_loc_dict[rt_stps[i]],'--->',stops_loc_dict[rt_stps[i+1]])
 			subway_link = LineString([stops_loc_dict[rt_stps[i]], stops_loc_dict[rt_stps[i+1]]])
 			subway_link_gpd = gpd.GeoSeries(subway_link)
 			subway_link_gpd.plot(ax=ax, color='red')
@@ -84,5 +96,5 @@ for k in trip_stops.keys():
 
 		except KeyError:
 			continue
-
+	ni+=1
 plt.show()
